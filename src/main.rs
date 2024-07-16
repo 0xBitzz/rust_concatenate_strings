4ae3678168d3225a2ac7e80706ad6cd9061102d7
0xBitzz <successdaudu@gmail.com>
2024-07-16 11:07:40 +0100

    concatenate_strings

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+  let string1 = String::from("Hello");
+  let string2 = String::from(", Pakita");
+  let concatenated_string = concatenate_strings(&string1[..], &string2[..]);
+  println!("{}", concatenated_string);
+}
+
+fn concatenate_strings(s1: &str, s2: &str) -> String {
+  let mut result = String::new();
+  result.push_str(s1);
+  result.push_str(s2);
+  result
+}
